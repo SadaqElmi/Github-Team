25a3e94231ce64fad787ff065da32a22d60238b2
furqan117 <furqaanfarxaan2@gmail.com>
2025-01-15 14:34:45 +0300

    component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Afnan from "../components/Afnan_com";
+import Faruq from "../components/Faruq";
 
 const Home = () => {
   return (
     <div>
       <h1>This is Home Page </h1>
       <Afnan/>
+      <Faruq />
     </div>
   );
 };
