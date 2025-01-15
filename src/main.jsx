c3fb72d75cff3ec0957f31a5c0e6238521320318
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-01-15 17:02:44 +0300

    hero section

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import HeroSection from './HeroSection/Herosection.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <HeroSection />
   </StrictMode>,
 )
